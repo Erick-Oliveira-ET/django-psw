28ebf00ae308a4285b5130ee0e53a78d75a01bb6
Erick Oliveira <erickoliveirasystem@outlook.com>
2023-07-04 16:34:10 -0300

    feat: migrations

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+    essential = models.BooleanField(default=False)
+    planning_value = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.category
+    
+class Account(models.Model):
+    bank_choices = (
+        ('NU', 'Nubank'),
+        ('CE', "Caixa Economica")
+    )
+
+    type_choices = (('pp', 'Physical Person',), ('jp', 'Juridical Person'))
+
+    nickname = models.CharField(max_length=50)
+    bank = models.CharField(max_length=2, choices=bank_choices)
+    bank_type = models.CharField(max_length=2, choices=type_choices)
+    value = models.IntegerField()
+    icon = models.ImageField(upload_to="icons")
+
+    def __str__(self):
+        return self.nickname
+    
